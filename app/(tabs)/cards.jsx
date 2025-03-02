34a2b25f3c8f12682f43785c2b78c24448ee3f3f
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-03-01 18:11:20 -0600

    First commit with working cards

---
+++ app/(tabs)/cards.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import Swiper from 'react-native-deck-swiper';
+
+export default function CardsScreen() {
+  return (
+    <View style={styles.container}>
+      <Swiper
+        cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+        renderCard={(card) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text}>{card}</Text>
+            </View>
+          )
+        }}
+        onSwiped={(cardIndex) => { console.log(cardIndex) }}
+        onSwipedAll={() => { console.log('onSwipedAll') }}
+        cardIndex={0}
+        backgroundColor={'#4FD0E9'}
+        stackSize={3}>
+        <Button
+          onPress={() => { console.log('oulala') }}
+          title="Press me">
+          You can press me
+        </Button>
+      </Swiper>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
+});
